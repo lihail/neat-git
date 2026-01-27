58b70a90f0aeb8dd0871612384cb8316094bc79c
lihail <lihail@users.noreply.github.com>
2026-01-27 16:33:42 +0200

    Start the app maximized

--- electron/main.ts
+++ electron/main.ts
@@ -58,6 +58,8 @@ async function createWindow() {
     },
   });
 
+  win.maximize();
+
   // Test active push message to Renderer-process
   win.webContents.on("did-finish-load", () => {
     win?.webContents.send("main-process-message", new Date().toLocaleString());
